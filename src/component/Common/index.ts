776fe34dc478729c0a91d647f9aa7fdac3a52de8
bosungJang <bosung_jang@tmax.co.kr>
2024-10-07 09:34:07 +0900

    [fix] Typography 파일명 수정

--- src/component/Common/index.ts
+++ src/component/Common/index.ts
@@ -1,5 +1,7 @@
 import Tab from './Tabs';
 import Button from './Button';
 import TabPanel from './TabPanel';
+import Toggle from './Toggle';
+import Typography from './Typography';
 
-export { Tab, Button, TabPanel };
+export { Tab, Button, TabPanel, Toggle, Typography };
